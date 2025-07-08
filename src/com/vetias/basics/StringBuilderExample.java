32bf52144f3d2d9659e5f409de9911fb7a5cde7b
yuva1219 <yuvarajas24aid@vetias.ac.in>
2025-07-08 09:28:49 +0530

    NAME

---
+++ src/com/vetias/basics/StringBuilderExample.java
@@ -0,0 +1,18 @@
+public class StringBuilderExample {
+    public static void main(String[] args){
+    StringBuilder sb =new StringBuilder();
+
+    sb.append("Java ");
+    sb.append("is ");
+    sb.append("the ");
+    sb.append("most");
+    sb.append("used ");
+    sb.append("language");
+    sb.append("for ");
+    sb.append("Enterprise ");
+    sb.append("Applications");
+     System.out.println(sb.toString());  
+    }
+}
+
+
