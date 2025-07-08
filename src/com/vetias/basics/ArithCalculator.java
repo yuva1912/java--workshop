6437fa26350346dec0ebd48e25f2f5169f211a58
yuva1219 <yuvarajas24aid@vetias.ac.in>
2025-07-08 15:57:28 +0530

    FILE

---
+++ src/com/vetias/basics/ArithCalculator.java
@@ -0,0 +1,22 @@
+   import java.util.Scanner;
+
+public class ArithCalculator {
+    public static void main(String[] args) {
+        int a=3,b=0;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        try{
+            if (b==0) 
+            {
+                throw new ArithmeticException("0 cant be a divisor");
+
+                
+            }
+        }catch(ArithmeticException e){
+                System.out.println(e.getMessage());
+        }
+            
+    }
+
+}
