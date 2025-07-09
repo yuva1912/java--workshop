b8e320b778cb261abfad0494d078a6aab9ab4719
yuva1219 <yuvarajas24aid@vetias.ac.in>
2025-07-09 11:35:08 +0530

    STREAM

---
+++ src/com/vetias/StandardLibs/FileCopyUsingBinary.java
@@ -0,0 +1,25 @@
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopyUsingBinary {
+    public static void main(String[] args) {
+        try(FileInputStream inputStream=new FileInputStream("Screenshot (1).png");
+        FileOutputStream outputStream=new FileOutputStream("yuvan.png"))
+        {
+            byte[] chunk=new byte[1024];
+            int byteRead;
+            while((byteRead=inputStream.read(chunk))!=-1)
+            {
+                outputStream.write(chunk,0,byteRead);
+
+            }
+        }
+        catch(IOException e){
+            System.out.println("can't read the file");
+        }
+    }
+
+}
