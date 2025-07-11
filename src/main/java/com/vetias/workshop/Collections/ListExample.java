0a2a257cfbeb3a4f634ca8f66fba86b0e8f295f3
yuva1219 <yuvarajas24aid@vetias.ac.in>
2025-07-11 11:08:45 +0530

    YUVA

--- src/main/java/com/vetias/workshop/Collections/ListExample.java
+++ src/main/java/com/vetias/workshop/Collections/ListExample.java
@@ -1,20 +1,36 @@
- package com.vetias.workshop.Collections;
+package com.vetias.java.workshop.collections;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ListExample {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-        names.add("Raja");
-        names.add("Aadhav");
-        names.add("Yuva");
-        names.add("Mani");
-        names.add("Sudha");
-        System.out.println(names);
-        for(int i = 0;i< names.size();i++){
-            System.out.println(names.get(i));
-        }
-    }
+       List<String> names = new ArrayList<>();
+       names.add("sree");
+       names.add("krish");
+       names.add("anada");
+       names.add("dharan");
+       names.add("tr");
+       names.add("ranjith");
+       System.out.println(names);
+      
+       names.set(1,"ram");
+      
+       for(String name:names){
+        System.out.println(name);   
+       }
+       
+       System.out.println("another method:");
+       
+       for(int i=0;i<names.size();i++){
+        System.out.println(names.get(i));   
+       }
+    
+       names.forEach(System.out::println);
+       names.sort(Comparator.reverseOrder());
+       System.out.println(names);
 
+    
+}
 }
---
+++ src/main/java/com/vetias/workshop/Lambda/Square.java
@@ -0,0 +1,13 @@
+package com.vetias.java.workshop.lambda;
+
+public interface Square {
+
+    
+   
+  public int findSquare(int x);
+
+
+
+}
+ 
+    
