422541046035eb11a84ad05a29c3c3d8767c7b13
yuva1219 <yuvarajas24aid@vetias.ac.in>
2025-07-15 10:02:33 +0530

    JAVA

--- src/main/java/com/vetias/workshop/temperdata/DAO/OrganizationDao.java
+++ src/main/java/com/vetias/workshop/temperdata/DAO/OrganizationDao.java
@@ -1,29 +1,49 @@
 package com.vetias.workshop.temperdata.DAO;
 
+import java.beans.Statement;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class OrganizationDao {
-    public void createTable() {
-        JdbcDataSource h2DataSource = new JdbcDataSource();
-        h2DataSource.setURL("jdbc:h2:mem:tempdataDB");
-        h2DataSource.setUser("sa");
-        try (Connection dbconnection = h2DataSource.getConnection()) {
-            Statement statement = dbconnection.createStatement();
-               Statement.execute("""
-                CREATE TABLE IF NOT EXISTS organization (
-                    id INT AUTO_INCREMENT PRIMARY KEY,
-                    NAME VARCHAR(100),
-                    WEBSITE VARCHAR(100),
-                    EMAIL VARCHAR(100),
-                    PHONE VARCHAR(100),
-                    REGISTRATION_NO INT,   
-
-                       """);
-            
-        } catch (SQLException e) {
-           System.out.println("Error creating table"+SQLException);
-
+    public void createTable (Connection dbConnection) {
+        try(Statement statement = dbConnection.createStatement()) {
+                statement.execute("""
+                        CREATE TABLE Organization (
+                        id int AUTO_INCREMENT PRIMARY KEY,
+                        NAME VARCHAR(100),
+                        WEBSITE VARCHAR(100),
+                        EMAIL VARCHAR(100),
+                    CONTACT_NUMBER VARCHAR(100),
+                    REGISTRATION_NO INT,
+                    DESCRIPTION VARCHAR(255))
+                     """);
+        } catch (SQLException sqlException) {
+            System.err.println("Error creating Organization table: " + sqlException.getMessage());
         }
+            
+    
+      
+    }
+    public int save (Connection dbConnection, Organization vet){
+        try(PreparedStatement preparedStatement = dbConnection.prepareStatement("""
+            INSERT INTO Organization (NAME, WEBSITE, EMAIL, CONTACT_NUMBER, 
+            REGISTRATION_NO, DESCRIPTION) VALUES (?, ?, ?, ?, ?, ?)            
+                        """;)){
+                        preparedStatement.setString(1, vet.getName());
+                        preparedStatement.setString(2, vet.getWebsite());
+                        preparedStatement.setString(3, vet.getEmail());
+                        preparedStatement.setString(4, vet.getContactNumber()); 
+                        preparedStatement.setInt(5, vet.getRegistrationNo());
+                        preparedStatement.setString(6, vet.getDescription());
+                        PreparedStatement.executeUpdate();
+    
+        
+    } catch (SQLException sqlExceptionException) {
+        System.out.println("Error inserting into table: " + sqlExceptionException.getMessage());
+      
+    }
+    return 0; 
+    
     }
 
 }
