393d5f982789b22279c39c062c7277c9e6b499ad
yuva1219 <yuvarajas24aid@vetias.ac.in>
2025-07-15 11:11:26 +0530

    YUVA

--- src/main/java/com/vetias/workshop/temperdata/DAO/OrganizationDao.java
+++ src/main/java/com/vetias/workshop/temperdata/DAO/OrganizationDao.java
@@ -1,49 +1,49 @@
-package com.vetias.workshop.temperdata.DAO;
-
-import java.beans.Statement;
-import java.sql.PreparedStatement;
+package com.vetias.java.workshop.TempData.dao;
+import java.sql.Statement;
 import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
-public class OrganizationDao {
-    public void createTable (Connection dbConnection) {
-        try(Statement statement = dbConnection.createStatement()) {
-                statement.execute("""
-                        CREATE TABLE Organization (
-                        id int AUTO_INCREMENT PRIMARY KEY,
-                        NAME VARCHAR(100),
-                        WEBSITE VARCHAR(100),
-                        EMAIL VARCHAR(100),
+import org.h2.jdbcx.JdbcDataSource;
+
+import com.vetias.java.workshop.TempData.Beans.Organization;
+
+public class OrganizationDAO {
+    public void createTable(Connection dbConnection) {
+       
+        try ( Statement statement = dbConnection.createStatement()) {
+           
+            statement.execute(
+                """
+                CREATE TABLE organization (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    NAME VARCHAR(100),
+                    WEBSITE VARCHAR(100),
+                    EMAIL VARCHAR(100),
                     CONTACT_NUMBER VARCHAR(100),
-                    REGISTRATION_NO INT,
-                    DESCRIPTION VARCHAR(255))
-                     """);
+                    REGISTRATION_NO INT
+                )
+                """
+            );
         } catch (SQLException sqlException) {
-            System.err.println("Error creating Organization table: " + sqlException.getMessage());
+            System.out.println("Error Creating Table:" + sqlException);
         }
-            
-    
-      
     }
-    public int save (Connection dbConnection, Organization vet){
-        try(PreparedStatement preparedStatement = dbConnection.prepareStatement("""
-            INSERT INTO Organization (NAME, WEBSITE, EMAIL, CONTACT_NUMBER, 
-            REGISTRATION_NO, DESCRIPTION) VALUES (?, ?, ?, ?, ?, ?)            
-                        """;)){
-                        preparedStatement.setString(parameterIndex:1, vet.Name());
-                        preparedStatement.setString(parameterIndex:2, vet.Website());
-                        preparedStatement.setString(parameterIndex:3, vet.Email());
-                        preparedStatement.setString(parameterIndex:4, vet.ContactNumber()); 
-                        preparedStatement.setString(parameterIndex:5, vet.RegistrationNo());
-                        preparedStatement.setString(parameterIndex:6, vet.Description());
-                        PreparedStatement.executeUpdate();
-    
-        
-    } catch (SQLException sqlExceptionException) {
-        System.out.println("Error inserting into table: " + sqlExceptionException.getMessage());
-      
-    }
-    return 0; 
-
-    }
-
+    public int save(Connection dbConnection,Organization vet){
+        try(PreparedStatement preparedStatement = dbConnection.prepareStatement(
+                "INSERT INTO organization (NAME, WEBSITE, EMAIL, CONTACT_NUMBER, REGISTRATION_NO) VALUES (?, ?, ?, ?, ?)"
+        )) {
+            preparedStatement.setString(1, vet.name());
+            preparedStatement.setString(2, vet.website());
+            preparedStatement.setString(3, vet.email());
+            preparedStatement.setString(4, vet.contactNumber());
+            preparedStatement.setLong(5, vet.registrationNumber());
+            preparedStatement.setString(6, vet.description());
+            return preparedStatement.executeUpdate();
+        } catch (SQLException sqlException) {
+            System.out.println("Error Saving Organization: " + sqlException);
+            return 0;
+        }
+                                                     
+}
 }
